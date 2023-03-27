065e05e71f4fd976581f644bfc95ee18f9f052ac
Sagecore Technologies <cloud@sagecoretech.com>
2023-03-27 08:54:58 -0600

    --

--- arrayofnumbers.js
+++ arrayofnumbers.js
@@ -6,7 +6,7 @@ function add(arrayOfNumbers) {
     //preferred way
     for (var i in arrayOfNumbers) {
         var num = arrayOfNumbers[i]
-    }
+    } 
 
     //convenient way if value and index are needed
     arrayOfNumbers.map((value, index) => {
@@ -30,4 +30,23 @@ function add(arrayOfNumbers) {
 
 const myNumbers = [1,5,10,25,150]
 var total = add(myNumbers)
-console.log(total)
+console.log(total);
+
+const obj = {}
+obj["moby-dick"] = ["page 1 text.....","page 2 text...."]
+obj["another-book"] = ["page 1 text.....","page 2 text...."]
+obj["last-book"] = ["page 1 text.....","page 2 text...."]
+
+console.log(myNumbers);
+console.log(obj);
+
+console.log(obj["moby-dick"])
+
+for(var pagenumber in obj["another-book"]){
+  const page = obj["another-book"][pagenumber]
+  console.info("Page Number:",pagenumber,page)
+}
+
+obj["moby-dick"].map((page,pagenumber) => {
+  console.info("Page Number:",pagenumber,page)
+})
